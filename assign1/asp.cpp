08cbb77c22f099366dbdb21dd34e3cfbaba3fb35
khdrew <andrewex.lai@hotmail.com>
2017-03-23 13:31:14 +1300

    asp function creation

--- assign1/asp.cpp
+++ assign1/asp.cpp
@@ -1,14 +1,58 @@
 // asp.h
-
 #include "asp.h"
 
-SC_MODULE(asp)
-{
-    void asp_func();
-    
-    SC_CTOR(asp)
-    {
-		
-    }
-};
+template<int N>
+void asp<N>::asp_func(){
+	current_state = Idle;
+	busy.write(false);
+	Res_ready.write(false);
+	wait();
+	while(true){
+		if (valid.read()){
+			busy.write(true);
+			switch (current_state){
+				case Idle:
+					opcode = data_in.read() >> 22;
+					if (opcode == 0b0000){
+						store_init();
+					} else if (opcode == 0b0001){
+						data_count = (data_in.read() & 511)
+						current_state = Wait_Invoke;
+					}
+					break;
+				case Wait_Invoke:
+					store_invoke();
+					current_state = Wait_Data;
+					break;
+				case Wait_Data:
+					process_data();
+					current_state = Idle;
+				default:
+					current_state = Idle;
+					break;
+			}
+			busy.write(false);
+		}
+	}
+}
 
+void asp<N>::store_init(){
+	if (data_in.read() & (1 << 22)) {
+		for (int i = 0; i < N; i++){
+			B[i] = 0;
+		}
+	}else{
+		for (int i = 0; i < N; i++){
+			A[i] = 0;
+		}
+	}
+}
+
+void asp<N>::store_invoke(){
+	begin_address = (data_in.read() & 511);
+	end_address = ((data_in.read() >> 9) & 511);
+}
+
+void asp<N>::process_data(){
+	
+}
--- assign1/asp.h
+++ assign1/asp.h
@@ -1,13 +1,36 @@
 // asp.h
+#ifndef ASP_H
+#define ASP_H
+enum State {Idle = 1, Wait_Invoke, Wait_Data};
 
+template <int N>
 SC_MODULE(asp)
 {
+	sc_in<bool> valid;
+	sc_in<sc_bv<26> > data_in;
+	sc_out<sc_int<16> > data_out;
+	sc_out<bool> Res_ready;
+	sc_out<bool> busy;
+
+	State current_state;
+	int A[N];
+	int B[N];
+	int opcode;
+	int data_count;
+	int begin_address;
+	int end_address;
 
 	void asp_func();
+	void store_init();
+	void store_invoke();
+	void process_data();
 
 	SC_CTOR(asp)
 	{
-		SC_METHOD(asp_func);
+		SC_THREAD(asp_func);
+			sensitive << valid.pos();
 	}
 };
 
+
+#endif
